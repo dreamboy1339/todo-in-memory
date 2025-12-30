391c37e348f8175bd77d893b1d19619be4441244
fog <20683340+dreamboy1339@users.noreply.github.com>
2025-12-30 23:33:27 +0900

    모든 Todo 항목 조회 API 추가

--- src/main/java/com/asdf/todo/controller/TodoController.java
+++ src/main/java/com/asdf/todo/controller/TodoController.java
@@ -1,4 +1,35 @@
 package com.asdf.todo.controller;
 
+import com.asdf.todo.model.Todo;
+import com.asdf.todo.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos/v1")
 public class TodoController {
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping
+    @Operation(summary = "전체 작업 조회", description = "전체 작업 조회")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "204", description = "내용 없음")
+    })
+    public ResponseEntity<List<Todo>>  getAllTodos() {
+        List<Todo> todos = todoService.findAll();
+        if (todos == null || todos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(todos);
+    }
 }
